9f80daa8cc51580ae4ef314897a124d5b83bf244
Мамонтов Виктор Викторович <VViMamontov@sberbank.ru>
2021-05-25 16:13:10 +0300

    create ConsoleWorker class

---
+++ src/main/java/ru/sberbank/viktormamontov/ConsoleWorker.java
@@ -0,0 +1,79 @@
+package ru.sberbank.viktormamontov;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class ConsoleWorker {
+
+    private CityHandler cityHandler;
+    private Scanner scanner;
+    private List<City> cities;
+
+    public ConsoleWorker(List<String> lines) {
+        this.cityHandler = new CityHandlerImpl();
+        cities = cityHandler.parseLines(lines);
+        scanner = new Scanner(System.in);
+    }
+
+    public void showMenu() {
+        do {
+
+            System.out.println("Введите номер действия:");
+            System.out.println("1 - получить список городов");
+            System.out.println("2 - получить список городов, отсортированный по названию по убыванию");
+            System.out.println("3 - получить список городов, отсортированный по региону и по названию по убыванию");
+            System.out.println("4 - получить индекс города с наибольшим населением");
+            System.out.println("5 - получить список регионов с количеством их городов");
+            System.out.println("0 - нажмите для выхода");
+
+            String s = scanner.nextLine();
+            int number;
+            try {
+                number = Integer.parseInt(s);
+            } catch (NumberFormatException e) {
+                System.out.println("Введены некорректные данные, попробуйте снова.");
+                continue;
+            }
+
+            switch (number) {
+                case 1:
+                    System.out.println("========================");
+                    cities.forEach(System.out::println);
+                    System.out.println("========================");
+                    break;
+                case 2:
+                    System.out.println("========================");
+                    cityHandler.sortByName(cities);
+                    cities.forEach(System.out::println);
+                    System.out.println("========================");
+                    break;
+                case 3:
+                    System.out.println("========================");
+                    cityHandler.sortByDistrictAndName(cities);
+                    cities.forEach(System.out::println);
+                    System.out.println("========================");
+                    break;
+                case 4:
+                    System.out.println("========================");
+                    Map<Integer, Integer> cityWithLargestPopulation = cityHandler.findCityIndexWithLargestPopulation(cities);
+                    cityWithLargestPopulation.forEach((key, value) -> System.out.printf("[%d] = %d\n", key, value));
+                    System.out.println("========================");
+                    break;
+                case 5:
+                    System.out.println("========================");
+                    Map<String, Long> countCitiesByRegion = cityHandler.countCitiesByRegion(cities);
+                    countCitiesByRegion.forEach((key, value) -> System.out.println(key + " - " + value));
+                    System.out.println("========================");
+                    break;
+                case 0:
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Введены некорректные данные, попробуйте снова.");
+                    continue;
+            }
+
+        } while (true);
+    }
+}
--- src/main/java/ru/sberbank/viktormamontov/Main.java
+++ src/main/java/ru/sberbank/viktormamontov/Main.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -23,33 +22,8 @@ public class Main {
         }
         scanner.close();
 
-        CityHandlerImpl cityHandler = new CityHandlerImpl();
-        List<City> cities = cityHandler.parseLines(inputLines);
-        System.out.println("========================");
-
-
-        cities.forEach(System.out::println);
-        System.out.println("========================");
-
-        cityHandler.sortByName(cities);
-        cities.forEach(System.out::println);
-        System.out.println("========================");
-
-        cityHandler.sortByDistrictAndName(cities);
-        cities.forEach(System.out::println);
-        System.out.println("========================");
-
-        Map<Integer, Integer> cityWithLargestPopulation = cityHandler.findCityIndexWithLargestPopulation(cities);
-        cityWithLargestPopulation.forEach((key, value) -> System.out.printf("[%d] = %d\n", key, value));
-        System.out.println("========================");
-
-//        City cityWithLargestPopulation1 = cityHandler.findCityWithLargestPopulation(cities);
-//        System.out.println(cityWithLargestPopulation1);
-//        System.out.println("========================");
-
-        Map<String, Long> countCitiesByRegion = cityHandler.countCitiesByRegion(cities);
-        countCitiesByRegion.forEach((key, value) -> System.out.println(key + " - " + value));
-        System.out.println("========================");
+        ConsoleWorker consoleWorker = new ConsoleWorker(inputLines);
+        consoleWorker.showMenu();
 
     }
 }
